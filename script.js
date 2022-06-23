c61fe151f0adba5fee8b62f34da56aac20301d31
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-06-23 17:03:47 -0500

    wow i actually did it

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your JS goes here
+var container = document.createElement('div');
+// container.style.width = 100%
+
+document.body.append(container);
+
+function createCheckerBoard() {
+    for(var i = 0; i < 63; i++) {
+    var div = document.createElement('div');
+        div.style.width = '11.1%';
+        div.style.float = 'left';
+        div.style.paddingBottom = '11.1%';
+        container.append(div);
+
+        if (i % 2 === 0) {
+            div.style.background = 'red';
+        } else {
+            div.style.background = 'black';
+        }
+    }
+}
+
+createCheckerBoard();
