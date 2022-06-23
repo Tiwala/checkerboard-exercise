ff41e437fc1509eb19be88a98285cf570ccaf937
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-06-23 18:29:00 -0500

    THE GRADIENT

--- script.js
+++ script.js
@@ -6,23 +6,34 @@ var container = document.createElement('div');
 document.body.append(container);
 
 function createCheckerBoard() {
+    var randomColorOne = Math.ceil(Math.random() * 100);
+    var randomColorTwo = Math.ceil(Math.random() * 100);
+    var randomColorThree = Math.ceil(Math.random() * 100);
+    var randomColorFour = Math.ceil(Math.random() * (255 - 150) + 150);
+    var randomColorFive = Math.ceil(Math.random() * (255 - 150) + 150);
+    var randomColorSix = Math.ceil(Math.random() * (255 - 150) + 150);
     for (var i = 0; i < 63; i++) {
-    var randomColorOne = Math.ceil(Math.random() * 255);
-    var randomColorTwo = Math.ceil(Math.random() * 255);
-    var randomColorThree = Math.ceil(Math.random() * 255);
     var div = document.createElement('div');
         div.style.width = '11.1%';
         div.style.float = 'left';
         div.style.paddingBottom = '11.1%';
         container.append(div);
-        div.style.background = `rgb(${randomColorOne},${randomColorTwo},${randomColorThree})`;
+
+        if (i % 2 === 0) {
+            div.style.background = `rgb(${randomColorOne},${randomColorTwo},${randomColorThree})`;
+        } else {
+            div.style.background = `rgb(${randomColorFour},${randomColorFive},${randomColorSix})`;
+        }
         
 
-        // if (i % 2 === 0) {
-        //     // div.style.background = 'black';
-        // } else {
-        //     div.style.background = 'red';
-        // }
+        randomColorOne += 2;
+        randomColorTwo += 2;
+        randomColorThree += 2;
+        randomColorFour -= 2;
+        randomColorFive -= 2;
+        randomColorSix -= 2;
+
+
     }
 }
 
